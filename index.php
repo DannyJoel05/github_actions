ac9b06fcc8847eba6132934264d9f983d310e1af
Danny Joel <66756869+DannyJoel05@users.noreply.github.com>
2025-05-24 12:58:13 -0500

    commit 5

--- index.php
+++ index.php
@@ -15,6 +15,14 @@
     // This is a simple PHP script
     echo "<h1>Hello, World!</h1>";
     echo '¡Mi nombre de usuario es ' . $_ENV["USER"] . '!';
+    echo '¡Mi nombre de username es ' . $_ENV["username"] . '!';
+    echo '¡Mi nombre de password es ' . $_ENV["password"] . '!';
+    echo '¡Mi nombre de user es ' . $_ENV["USER"] . '!';
+    echo '¡Mi nombre de secrets.USERNAME es ' . $_ENV["secrets.USERNAME"] . '!';
+    echo '¡Mi nombre de secrets.PASSWORD es ' . $_ENV["secrets.PASSWORD"] . '!';
+    echo '¡Mi nombre de database es ' . $_ENV["database"] . '!';
+    echo '¡Mi nombre de vars.LANGUAGE es ' . $_ENV["vars.LANGUAGE"] . '!';
+    echo '¡Mi nombre de vars.DESARROLLADOR es ' . $_ENV["vars.DESARROLLADOR"] . '!';
     ?>
     <script src="main.js"></script>
 </body>
