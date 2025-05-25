4c9c598b25a2f2d673887815a20c7454423177f6
Danny Joel <66756869+DannyJoel05@users.noreply.github.com>
2025-05-24 20:25:27 -0500

    commit 24

--- index.php
+++ index.php
@@ -1,36 +1,25 @@
+<?php
+// Cargar el archivo .env
+$env = parse_ini_file(__DIR__ . '/.env');
 
+// Cargar variables al entorno
+foreach ($env as $key => $value) {
+    $_ENV[$key] = $value;
+    putenv("$key=$value"); // Opcional, para que esté disponible también para llamadas al sistema
+}
 
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-
-<body>
-    <h1>Bienvenidos Danny 2</h1>
-    <?php
-    // Cargar el archivo .env
-    $env = parse_ini_file(__DIR__ . '/.env');
-    echo '<pre>' . print_r($env, true) . '</pre>';
-    echo $env;
-    // Load environment variables from .env file
-    // This is a simple PHP script
-    echo "<h1>Hello, World!</h1>";
-    echo '¡Mi nombre de usuario es ' . $_ENV["USER"] . '!';
-    echo '¡Mi nombre de username es ' . $_ENV["username"] . '!';
-    echo '¡Mi nombre de password es ' . $_ENV["password"] . '!';
-    echo '¡Mi nombre de user es ' . $_ENV["USER"] . '!';
-    echo '¡Mi nombre de secrets.USERNAME es ' . $_ENV["secrets.USERNAME"] . '!';
-    echo '¡Mi nombre de secrets.PASSWORD es ' . $_ENV["secrets.PASSWORD"] . '!';
-    echo '¡Mi nombre de database es ' . $_ENV["database"] . '!';
-    echo '¡Mi nombre de vars.LANGUAGE es ' . $_ENV["vars.LANGUAGE"] . '!';
-    echo '¡Mi nombre de vars.DESARROLLADOR es ' . $_ENV["vars.DESARROLLADOR"] . '!';
-    ?>
-    <script src="main.js"></script>
-</body>
-
-</html>
+// Mostrar contenido de $env (debug)
+echo '<pre>' . print_r($env, true) . '</pre>';
+// Load environment variables from .env file
+// This is a simple PHP script
+echo "<h1>Hello, World!</h1>";
+echo '¡Mi nombre de usuario es ' . $_ENV["USER"] . '!';
+echo '¡Mi nombre de username es ' . $_ENV["username"] . '!';
+echo '¡Mi nombre de password es ' . $_ENV["password"] . '!';
+echo '¡Mi nombre de user es ' . $_ENV["USER"] . '!';
+echo '¡Mi nombre de secrets.USERNAME es ' . $_ENV["secrets.USERNAME"] . '!';
+echo '¡Mi nombre de secrets.PASSWORD es ' . $_ENV["secrets.PASSWORD"] . '!';
+echo '¡Mi nombre de database es ' . $_ENV["database"] . '!';
+echo '¡Mi nombre de vars.LANGUAGE es ' . $_ENV["vars.LANGUAGE"] . '!';
+echo '¡Mi nombre de vars.DESARROLLADOR es ' . $_ENV["vars.DESARROLLADOR"] . '!';
+?>
